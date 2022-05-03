58508afb53b2803192b3ead6a8fccabb9460b46c
Karl Horky <karl.horky@gmail.com>
2022-05-03 12:51:13 +0200

    Add first working version

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('string of text');
+import emoji from 'node-emoji';
+
+if (process.argv.length < 3) {
+  console.log(emoji.random().emoji);
+} else if (!emoji.hasEmoji(process.argv[2])) {
+  console.log('Invalid emoji');
+} else {
+  console.log(emoji.get(process.argv[2]));
+}
+
+// if (process.argv.length > 2 && emoji.hasEmoji(process.argv[2])) {
+//   console.log(emoji.get(process.argv[2]));
+// } else if (process.argv.length > 2 && !emoji.hasEmoji(process.argv[2])) {
+//   console.log('Invalid emoji');
+// } else {
+//   console.log(emoji.random().emoji);
+// }
